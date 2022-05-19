716a5dda74c767b8284fd8039d4e4ddb7bf2a742
minjeong073 <kim.mj80730@gmail.com>
2022-05-19 15:19:59 +0900

    [0518] [Java Standard] example

---
+++ JAVA/src/standard/ch2Variable/example/VarEx1.java
@@ -0,0 +1,21 @@
+package standard.ch2Variable.example;
+
+public class VarEx1 {
+    public static void main(String[] args) {
+        // 변수 : 단 하나의 값을 저장할 수 있는 메모리 공간
+        // 변수의 초기화 : 변수를 사용하기 전에 처음으로 값을 저장하는 것
+
+        int year = 0;
+        int age = 14;
+
+        System.out.println(year);
+        System.out.println(age);
+
+        year = age + 2000;
+        age = age + 1;
+
+        System.out.println(year);
+        System.out.println(age);
+
+    }
+}
