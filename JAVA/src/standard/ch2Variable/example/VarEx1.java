17b4e9c4684bcbc88dbccdabbe350463a77968aa
minjeong073 <kim.mj80730@gmail.com>
2022-05-19 15:38:07 +0900

    [0519] [Java Standard] Ch2 variable example

--- JAVA/src/standard/ch2Variable/example/VarEx1.java
+++ JAVA/src/standard/ch2Variable/example/VarEx1.java
@@ -1,7 +1,9 @@
 package standard.ch2Variable.example;
 
 public class VarEx1 {
+
     public static void main(String[] args) {
+
         // 변수 : 단 하나의 값을 저장할 수 있는 메모리 공간
         // 변수의 초기화 : 변수를 사용하기 전에 처음으로 값을 저장하는 것
 
--- JAVA/src/standard/ch2Variable/example/VarEx2.java
+++ JAVA/src/standard/ch2Variable/example/VarEx2.java
@@ -3,6 +3,7 @@ package standard.ch2Variable.example;
 public class VarEx2 {
 
     public static void main(String[] args) {
+
         int x = 10, y = 20;
         int tmp = 0;
 
