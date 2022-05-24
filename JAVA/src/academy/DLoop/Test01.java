a4a98e9995a9372fa2e1724d9857d8d9a4e4e06b
minjeong073 <kim.mj80730@gmail.com>
2022-05-24 15:11:11 +0900

    [0524] DLoop update

--- JAVA/src/academy/DLoop/Test01.java
+++ JAVA/src/academy/DLoop/Test01.java
@@ -74,6 +74,16 @@ public class Test01 {
 			}
 			System.out.println();
 		}
+
+//		for (int i = 1; i <= 5; i++) {
+//			for (int j = 1; j <= 5-i; j++) {
+//				System.out.print(" ");
+//			}
+//			for (int j = 1; j <= i*2-1; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
 		
 		
 	}
