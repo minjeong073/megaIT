8b2656b5ce81cc0b1d6804e83d0e7f3bbaaea322
minjeong073 <kim.mj80730@gmail.com>
2022-05-30 00:50:55 +0900

    [0527] array hw update

--- JAVA/src/academy/array/Test03.java
+++ JAVA/src/academy/array/Test03.java
@@ -45,18 +45,37 @@ public class Test03 {
 			System.out.print(arr[i] + " ");
 		}
 		System.out.println();
+		System.out.println();
 		
 		
 		System.out.println("3. 없는 수 찾기");
 		int[] numbers2 = {4, 6, 3, 8, 2, 9, 1, 5};
-		
+
+		boolean[] numbers2Check = new boolean[10];
+
 		for (int i = 0; i < numbers2.length; i++) {
-			
-			
+			numbers2Check[numbers2[i]] = true;
 		}
-		
-		
-		
-		
+
+		for (int i = 1; i < numbers2Check.length; i++) {
+			if (!numbers2Check[i]) {
+				System.out.println("없는 수는 " + i + " 입니다");
+			}
+		}
+		System.out.println();
+
+		System.out.println("4. 빈도수 구하기");
+		int[] numbers3 = {5, 3, 2, 1, 2, 4, 3, 3, 2, 1};
+
+		int[] numbers3Check = new int[6];
+
+		for (int i = 0; i < numbers3.length; i++) {
+			numbers3Check[numbers3[i]] ++;
+		}
+
+		for (int i = 1; i < numbers3Check.length; i++) {
+			System.out.println(i + " : " + numbers3Check[i]);
+		}
+
 	}
 }
