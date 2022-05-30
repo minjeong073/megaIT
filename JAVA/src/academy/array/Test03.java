cdd263605129af6491e63d74a3a2724eecd38ad2
minjeong073 <kim.mj80730@gmail.com>
2022-05-30 16:25:55 +0900

    [0530] array hw update

--- JAVA/src/academy/array/Test03.java
+++ JAVA/src/academy/array/Test03.java
@@ -32,19 +32,37 @@ public class Test03 {
 		
 		
 		System.out.println("2. 셔플");
+		/*
+		다만 제가 반복문을 100번 반복한 이유는 좀더 많은 횟수를
+		섞기위해서 만들어 놓은 틀이에요.
+		i가 꼭 index에서만 사용될 필요는 없습니다
+
+		셔플이라는 특성을 잘 고민해 보시고 반복횟수를 늘려서
+		다 많이 섞일 수 있도록 고민해 보면 좋을 것 같아요!!
+		 */
 		Random rand = new Random();
 		
 		int[] randomArray = new int[10];
 		for (int i = 0; i < randomArray.length; i++) {
 			randomArray[i] = (i + 1);
 		}
-		
-		for (int i = 0; i < 10; i++) {
-			int randomNumber = rand.nextInt(10);
-			tmp = randomArray[i];
-			randomArray[i] = randomArray[randomNumber];
-			randomArray[randomNumber] = tmp;
+
+//		for (int i = 0; i < 100; i++) {
+//			int randomNum = rand.nextInt(10);
+//			for (int j = 0; j < 10; j++) {
+//				tmp = randomArray[j];
+//				randomArray[j] = randomArray[randomNum];
+//				randomArray[randomNum] = tmp;
+//			}
+//		}
+
+		for (int i = 0; i < 100; i++) {
+			int randomNum = rand.nextInt(10);
+			tmp = randomArray[0];
+			randomArray[0] = randomArray[randomNum];
+			randomArray[randomNum] = tmp;
 		}
+
 		
 		printArray(randomArray);
 		System.out.println();
@@ -53,15 +71,16 @@ public class Test03 {
 		System.out.println("3. 없는 수 찾기");
 		int[] numbers2 = {4, 6, 3, 8, 2, 9, 1, 5};
 
-		boolean[] numbers2Check = new boolean[10];
+		boolean[] numbers2Check = new boolean[9];
 
 		for (int i = 0; i < numbers2.length; i++) {
-			numbers2Check[numbers2[i]] = true;
+			numbers2Check[numbers2[i] - 1] = true;
 		}
 
 		for (int i = 1; i < numbers2Check.length; i++) {
 			if (!numbers2Check[i]) {
-				System.out.println("없는 수는 " + i + " 입니다");
+				System.out.println("없는 수는 " + (i + 1) + " 입니다");
+				break;
 			}
 		}
 		System.out.println();
@@ -70,14 +89,14 @@ public class Test03 {
 		System.out.println("4. 빈도수 구하기");
 		int[] numbers3 = {5, 3, 2, 1, 2, 4, 3, 3, 2, 1};
 
-		int[] numbers3Check = new int[6];
+		int[] numbers3Check = new int[5];
 
 		for (int i = 0; i < numbers3.length; i++) {
-			numbers3Check[numbers3[i]] ++;
+			numbers3Check[numbers3[i] - 1] ++;
 		}
 
-		for (int i = 1; i < numbers3Check.length; i++) {
-			System.out.println(i + " : " + numbers3Check[i]);
+		for (int i = 0; i < numbers3Check.length; i++) {
+			System.out.println((i + 1) + " : " + numbers3Check[i]);
 		}
 
 	}
