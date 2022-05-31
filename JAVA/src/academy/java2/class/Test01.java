7260c74534b830c82474e0392249b37b7bc8de92
김민정 <91234288+minjeong073@users.noreply.github.com>
2022-05-31 18:48:33 +0900

    Create Test01.java

---
+++ JAVA/src/academy/java2/class/Test01.java
@@ -0,0 +1,31 @@
+package test.test02;
+
+public class Test01 {
+	public static void main(String[] args) {
+		
+		Circle circle = new Circle();
+		
+		circle.radius = 8;
+		
+		System.out.println("원의 넓이 : " + circle.calculateArea() 
+		+ "\n원의 둘레 : " + circle.calculateCircumference() 
+		+ "\n원 25개 넓이 : " + circle.calculateArea(25));
+		System.out.println();
+		
+		
+		Product product = new Product();
+		
+		product.name = "새우깡";
+		product.price = 1300;
+		product.expiration = "2021-03-02";
+		
+		product.printInfo();
+		System.out.println();
+		
+		System.out.println("제품 5개의 가격 : " + product.calculatePrice(5)
+				+ "\n제품 13개의 가격 : " + product.calculatePrice(13));
+		product.isAvailable("2021-04-03");
+		
+	}
+
+}
