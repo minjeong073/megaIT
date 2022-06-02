1ccfbaf5ffa0dcc39d2e933d8d3e6233874d7739
김민정 <91234288+minjeong073@users.noreply.github.com>
2022-06-02 18:48:43 +0900

    [0602] construct class test01

---
+++ JAVA/src/academy/java2/construct/test02/Test02.java
@@ -0,0 +1,30 @@
+package academy.java2.construct.test02;
+
+public class Test02 {
+	public static void main(String[] args) {
+		
+		Student student1 = new Student("김민정", "@@대학교", "010-0000-1111", "java");
+		
+		student1.printInfo();
+		
+		student1.changeUniv("##대학교");
+		student1.printInfo();
+		
+		student1.sendMessage();
+		student1.sendMessage("잘하자!");
+		System.out.println();
+		
+		
+		Galaxy galaxy = new Galaxy("삼성", "Gallaxy");
+		
+		galaxy.setPhoneNumber("010-1111-2222");
+		galaxy.setSerialNumber("123456789");
+		
+		System.out.println("전화 번호 : " + galaxy.getPhoneNumber());
+		System.out.println("일련 번호 : " + galaxy.getSerialNumber());
+		
+		galaxy.call("010-2222-3333");
+		galaxy.message("010-3333-4444", "안녕?");
+		
+	}
+}
