843ceb6f539e62914971bbb81c7a088001d088df
김민정 <91234288+minjeong073@users.noreply.github.com>
2022-06-08 17:44:48 +0900

    [0608] inheritance test01 class
    IPodTouch.java

---
+++ JAVA/src/academy/java2/inheritance/test01/IPodTouch.java
@@ -0,0 +1,29 @@
+package academy.java2.inheritance.test01;
+
+public class IPodTouch {
+
+	private String maker;
+    private String model;
+
+    public IPodTouch() {
+        this.maker = "apple";
+        this.model = "iPodTouch";
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void internetBrowsing(String url) {
+        System.out.println(url + " 로딩중...");
+        System.out.println("로딩 완료");
+    }
+
+    public void playMusic(String musicName) {
+        System.out.println(musicName + " 재생 시작");
+    }
+
+    public String infoString() {
+        return this.maker + " - " + this.model;
+    }
+}
