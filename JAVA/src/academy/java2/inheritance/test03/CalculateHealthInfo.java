20f04be5b78ac6f3eb5b66180f1f3d53ceb590b9
김민정 <91234288+minjeong073@users.noreply.github.com>
2022-06-09 16:38:53 +0900

    [0609] inheritance test03 update

--- JAVA/src/academy/java2/inheritance/test03/CalculateHealthInfo.java
+++ JAVA/src/academy/java2/inheritance/test03/CalculateHealthInfo.java
@@ -10,15 +10,18 @@ public class CalculateHealthInfo extends HealthInfo{
 	}
 
 	public void calculateStandardWeight() {
-		this.standardWeight = (super.getHeight() - 100) * 0.9;
+// 		this.standardWeight = (super.getHeight() - 100) * 0.9;
 //		this.standardWeight = (super.height - 100) * 0.9;
+		this.standardWeight = (this.height - 100) * 0.9;
 	}
 	
-	public void calculateBmi() {
-		double bmiPercent = (super.getWeight() - this.standardWeight) 
-				/ this.standardWeight * 100;
+	public double calculateBmi() {
+// 		double bmiPercent = (super.getWeight() - this.standardWeight) 
+// 				/ this.standardWeight * 100;
 //		double bmiPercent = (super.weight - this.standardWeight) 
 //				/ this.standardWeight * 100;
+		double bmiPercent = (this.weight - calculateStandardWeight())
+				/ calculateStandardWeight() * 100;
 		
 		if (bmiPercent < 10) {
 			this.bmi = "정상";
@@ -27,15 +30,18 @@ public class CalculateHealthInfo extends HealthInfo{
 		} else {
 			this.bmi = "정상";
 		}
-		
+		return this.bmi;
 	}
 
 	@Override
 	public void showInfo() {
-		System.out.println(super.getName() + "님의 신장 " + super.getHeight()
-				+ ", 몸무게 " + super.getWeight() + "kg => " + this.bmi + "입니다.");
+// 		System.out.println(super.getName() + "님의 신장 " + super.getHeight()
+// 				+ ", 몸무게 " + super.getWeight() + "kg => " + this.bmi + "입니다.");
 //		System.out.println(super.name + "님의 신장 " + super.height 
 //				+ ", 몸무게" + super.weight + "kg => " + this.bmi + "입니다.");
+		System.out.println(this.name + "님의 신장 " + this.height
+				+ ", 몸무게" + this.weight + "님의 신장 " + "kg => " + calculateBmi() + "입니다.");
+
 	}
 
 }
