d6b0d5347ff632bf2ddffe01a38f3a3656af3cd7
김민정 <91234288+minjeong073@users.noreply.github.com>
2022-07-08 18:48:41 +0900

    [0708] database ex01 update

--- DATABASE/table/ex01.sql
+++ DATABASE/table/ex01.sql
@@ -18,3 +18,21 @@ DESC `person`;
 
 -- 테이블 삭제
 DROP TABLE `person`;
+
+-- 테이블 수정하기
+--  1. 테이블 이름 변경
+ALTER TABLE `person` RENAME `user`;
+
+DESC `user`;
+
+-- 2. 항목 추가 (email)
+ALTER TABLE `user` ADD COLUMN `email` varchar(16) NULL;
+-- NULL 쓸 수도 쓰지 않을 수도 있음
+
+
+-- 3. 컬럼 수정 (email 32자 까지) -> 전체 내용 수정
+ALTER TABLE `user` MODIFY COLUMN `email` varchar(32) NULL;
+
+
+-- 4. 컬럼 이름 수정 + 타입 수정 (birth (int) -> yyyymmdd (char))
+ALTER TABLE `user` CHANGE COLUMN `birth` `yyyymmdd` char(8) NOT NULL;
