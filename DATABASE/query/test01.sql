d3dedfa781ec1f9fc529f35aa3d216c12fe700f2
minjeong073 <kim.mj80730@gmail.com>
2022-07-09 20:58:45 +0900

    [0709] database query update file

---
+++ DATABASE/query/test01.sql
@@ -0,0 +1,27 @@
+DESC `score`;
+
+-- 1. 개인 성적 입력
+INSERT INTO `score`
+(`name`, `year`, `semester`, `term`, `subject`, `score`, `createdAt`, `updatedAt`)
+VALUE
+('유재석', 2020, 1, '중간고사', '국어', 80, now(), now());
+
+INSERT INTO `score`
+(`name`, `year`, `semester`, `term`, `subject`, `score`, `createdAt`, `updatedAt`)
+VALUE
+('유재석', 2020, 1, '중간고사', '영어', 90, now(), now());
+
+INSERT INTO `score`
+(`name`, `year`, `semester`, `term`, `subject`, `score`, `createdAt`, `updatedAt`)
+VALUE
+('유재석', 2020, 1, '중간고사', '수학', 95, now(), now());
+
+SELECT * FROM `score`;
+
+-- 2. 성적 입력 다량
+INSERT INTO `score`
+(`name`, `year`, `semester`, `term`, `subject`, `score`, `createdAt`, `updatedAt`)
+VALUES
+('조세호', 2020, 1, '중간고사', '국어', 75, now(), now()),
+('조세호', 2020, 1, '중간고사', '영어', 80, now(), now()),
+('조세호', 2020, 1, '중간고사', '수학', 95, now(), now());
