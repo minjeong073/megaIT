f9ef50602bdb779717f2f61a035f1a49c6dc3318
minjeong073 <kim.mj80730@gmail.com>
2022-07-19 16:38:02 +0900

    [0719] servlet servlet ex update

--- Servlet/src/main/java/com/minjeong/servlet/servlet/ex/Ex01Controller.java
+++ Servlet/src/main/java/com/minjeong/servlet/servlet/ex/Ex01Controller.java
@@ -5,37 +5,36 @@ import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Ex01Controller extends HttpServlet{
 
-	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-		// response header 조작
-		
-		// 어떤 형태의 데이터 인지 알려줌 데이터의 데이터
-		response.setCharacterEncoding("utf-8");	// 내가 전달할 charSet 이 무엇인지 알려줌
-		
-		// 데이터 타입
-		response.setContentType("text/plain");	// MIME 표준으로 정해짐
-		
-		// 현재 날짜와 시간 보여주기
-		// 2022-07-18 18:19:45
-		// Date class 활용
-		Date now = new Date();
-		
-		// response body 에 값을 쓰기 위해 PrintWriter class 활용
-		PrintWriter out = response.getWriter();
-		out.println(now);
-		
-		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		String dateTimeString = formatter.format(now);
-				
-		out.println(dateTimeString);
-	}
-	
+        // response header 조작
+
+        // 어떤 형태의 데이터 인지 알려줌 데이터의 데이터
+        response.setCharacterEncoding("utf-8");	// 내가 전달할 charSet 이 무엇인지 알려줌
+
+        // 데이터 타입
+        response.setContentType("text/plain");	// MIME 표준으로 정해짐
+
+        // 현재 날짜와 시간 보여주기
+        // 2022-07-18 18:19:45
+        // Date class 활용
+        Date now = new Date();
+
+        // response body 에 값을 쓰기 위해 PrintWriter class 활용
+        PrintWriter out = response.getWriter();
+        out.println(now);
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String dateTimeString = formatter.format(now);
+
+        out.println(dateTimeString);
+    }
+
 }
--- Servlet/src/main/java/com/minjeong/servlet/servlet/ex/HelloController.java
+++ Servlet/src/main/java/com/minjeong/servlet/servlet/ex/HelloController.java
@@ -1,8 +1,8 @@
-package com.minjeong.servlet.test;
+package com.minjeong.servlet.servlet.ex;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
