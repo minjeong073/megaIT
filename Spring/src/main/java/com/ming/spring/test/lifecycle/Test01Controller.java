47671ad75343fb6ef6276de13187f3bf2f16a4f8
김민정 <91234288+minjeong073@users.noreply.github.com>
2022-08-03 16:06:31 +0900

    [0802] spring lifecycle test01

---
+++ Spring/src/main/java/com/ming/spring/test/lifecycle/Test01Controller.java
@@ -0,0 +1,31 @@
+package com.ming.spring.test.lifecycle;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/lifecycle/test01")
+public class Test01Controller {
+	
+	@RequestMapping("/1")
+	@ResponseBody
+	public String stringResponse() {
+		return "테스트 프로젝트 완성";
+	}
+	
+	@RequestMapping("/2")
+	@ResponseBody
+	public Map<String, Integer> mapResponse() {
+		Map<String, Integer> map = new HashMap<>();
+		
+		map.put("국어", 80);
+		map.put("수학", 90);
+		map.put("영어", 85);
+		
+		return map;
+	}
+}
