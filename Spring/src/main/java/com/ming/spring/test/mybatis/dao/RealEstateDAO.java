d2e32a2c07648f70b85c728a1653e33a874cb5c2
김민정 <91234288+minjeong073@users.noreply.github.com>
2022-08-08 17:17:29 +0900

    [0805] spring database realEstate

---
+++ Spring/src/main/java/com/ming/spring/test/mybatis/dao/RealEstateDAO.java
@@ -0,0 +1,37 @@
+package com.ming.spring.test.mybatis.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import com.ming.spring.test.mybatis.model.RealEstate;
+
+@Repository
+public interface RealEstateDAO {
+
+	// 1. id 로 select
+	public RealEstate selectRealEstate(@Param("id") int id);
+	
+	// 2. 월세 조건 select
+	public List<RealEstate> selectRealEstateAsRent(@Param("rent") int rent);
+	
+	// 3. 복합 조건 select
+	public List<RealEstate> selectMatchRealEstate(@Param("area") int area, @Param("price") int price);
+	
+	
+	// insert
+	// 1. 객체로 insert 하기
+	public int insertRealEstateByObject(RealEstate realEstate);
+	
+	// 2. field 로 insert 하기
+	public int insertRealEstate(
+			@Param("realtorId") int realtorId
+			, @Param("address") String address
+			, @Param("area") int area
+			, @Param("type") String type
+			, @Param("price") int price
+			, @Param("rentPrice") int rentPrice);
+
+	
+}
